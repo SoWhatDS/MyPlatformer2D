dec50846087ead6cbca9409df706fe5c70885f96
SoWhatDS <dssergeev123@gmail.com>
2022-07-12 19:23:33 +0300

    HomeWork6

---
+++ Assets/PlatformerGB/Scripts/GeneratorLevel2/RootGenerateLevel2.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Platformer2D
+{
+    public class RootGenerateLevel2 : MonoBehaviour
+    {
+        [SerializeField] private GenerateLevelView _generateLevelView;
+        private GenerateLevelController _generateLevelController;
+
+        private void Awake()
+        {
+            _generateLevelController = new GenerateLevelController(_generateLevelView);
+            _generateLevelController.Awake();
+        }
+    }
+}
